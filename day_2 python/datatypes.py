668847d26dcbc9365ea39f83a1e207d370f8bf64
Saad Aman Alvi <saadamaan680@gmail.com>
2025-07-31 21:44:45 +0500

    Add files via upload

---
+++ day_2 python/datatypes.py
@@ -0,0 +1,16 @@
+# integer that can store only numeric value 
+a = 1   
+
+# float that can store both numeric or decimal valure
+b = 2.2
+
+# string that can store all but rule is start and close in double quotes
+c = "saad aman alvi" 
+
+# boolean that can store one true or false "decision making" true = 1 , False = 0
+d = True
+e = False
+
+# None represent no value null
+f = None
+print(a+b+d)
---
+++ day_2 python/variables.py
@@ -0,0 +1,5 @@
+a = 12   #a is identifier / variable that can store assign value
+b = 12.2
+c="saad"  #string also display in double quotes
+c = saad  #identifier can not assign each other 
+print(a+b)
